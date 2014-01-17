da81a49ef41e74f0e227a7dcf5a8f84cdd29db13
Will Fairclough <wfairclough@gmail.com>
2014-01-17 17:14:21 -0500

    Adding XML parser for levels.

---
+++ WhenAmI/LevelXmlParser.h
@@ -0,0 +1,13 @@
+//
+//  LevelXmlParser.h
+//  WhenAmI
+//
+//  Created by Will Fairclough on 2014-01-17.
+//  Copyright (c) 2014 Chicken Waffle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelXmlParser : NSXMLParser
+
+@end
---
+++ WhenAmI/LevelXmlParser.m
@@ -0,0 +1,13 @@
+//
+//  LevelXmlParser.m
+//  WhenAmI
+//
+//  Created by Will Fairclough on 2014-01-17.
+//  Copyright (c) 2014 Chicken Waffle. All rights reserved.
+//
+
+#import "LevelXmlParser.h"
+
+@implementation LevelXmlParser
+
+@end
