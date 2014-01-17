e894965a1991e6012034eb02b214df596a629750
Kat Butler <kat.a.butler@gmail.com>
2014-01-17 18:29:02 -0500

    Created GameConstants and started implementing collectable

--- WhenAmI/CollectableNode.m
+++ WhenAmI/CollectableNode.m
@@ -10,4 +10,15 @@
 
 @implementation CollectableNode
 
+//self = [SKSpriteNode spriteNodeWithImageNamed:@"ball2small"];
+
+-(id) init {
+    if (self = [super initWithImageNamed:@"energy_collectable"]) {
+        // Init stuff here
+        // self.physicsBody.restitution = 1.0; // Stuff like this here but you dont need restitution for this one
+        
+    }
+    return self;
+}
+
 @end
---
+++ WhenAmI/GameConstants.h
@@ -0,0 +1,17 @@
+//
+//  GameConstants.h
+//  WhenAmI
+//
+//  Created by Kat Butler on 2014-01-17.
+//  Copyright (c) 2014 Chicken Waffle. All rights reserved.
+//
+
+#ifndef WhenAmI_GameConstants_h
+#define WhenAmI_GameConstants_h
+
+
+static const uint32_t ballConst =  0x1 << 0;
+static const uint32_t energyConst =  0x1 << 1;
+
+
+#endif
