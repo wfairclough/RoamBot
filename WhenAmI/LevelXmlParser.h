5169a0ca8283bcdb02587aeb05e09816952f0a1d
Will Fairclough <wfairclough@gmail.com>
2014-01-17 17:39:28 -0500

    Added a level xml parser implementation to read xml level.

--- WhenAmI/LevelXmlParser.h
+++ WhenAmI/LevelXmlParser.h
@@ -8,6 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
-@interface LevelXmlParser : NSXMLParser
+
+#pragma mark - Level Delegate for Setup within the GameScene
+
+@protocol LevelSetupDelegate <NSObject>
+@optional
+- (void) setupLevelNumber:(NSInteger)levelNum;
+- (void) setupWorldType:(NSString*)worldType;
+
+- (void) setupPlankWithXPosition:(float)x yPosition:(float)y rotationAngle:(float)roation  powered:(BOOL)powered;
+- (void) setupBallWithXPosition:(float)x yPosition:(float)y;
+
+@end
+
+
+#pragma mark - LevelXmlParser Interface
+
+@interface LevelXmlParser : NSXMLParser <NSXMLParserDelegate>
+
+@property (nonatomic, weak) id <LevelSetupDelegate> setupDelegate;
 
 @end
